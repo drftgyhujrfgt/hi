dbfcd3bd2d87892b1295886c58b407f23b0d6545
drftgyhujrfgt <gs1krbaby01@hotmail.com>
2024-05-14 16:34:29 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const insertionSortRecursive = (arr, n = arr.length) => {
-  if (n <= 1) {
-    return arr;
-  }
-  insertionSortRecursive(arr, n - 1);
-  const last = arr[n - 1];
-  let j = n - 2;
-  while (j >= 0 && arr[j] > last) {
-    arr[j + 1] = arr[j];
-    j--;
-  }
-  arr[j + 1] = last;
-  return arr;
-};
+console.log("hi world");
